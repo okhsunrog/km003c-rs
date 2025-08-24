3a31f7b609611303121fc92483e29385b73b145a
okhsunrog <me@okhsunrog.dev>
2025-08-24 21:27:25 +0300

    tests...

---
+++ km003c-lib/examples/test_main_extraction.rs
@@ -0,0 +1,59 @@
+use km003c_lib::packet::{Attribute, RawPacket};
+use pcap_parser::traits::PcapReaderIterator;
+use pcap_parser::{*, Block};
+use std::fs::File;
+use bytes::Bytes;
+
+fn main() {
+    let file = File::open("matching_record/wireshark_0.7.pcapng").unwrap();
+    let mut reader = PcapNGReader::new(65536, file).expect("PcapNGReader");
+    let mut total_packets = 0;
+    let mut pd_packets = 0;
+    
+    loop {
+        match reader.next() {
+            Ok((offset, block)) => {
+                match block {
+                    PcapBlockOwned::NG(Block::EnhancedPacket(epb)) => {
+                        if epb.data.len() > 27 {
+                            if epb.data[16] == 0x81 {
+                                total_packets += 1;
+                                let km_payload = epb.data[27..].to_vec();
+                                
+                                // Try to parse as KM003C packet
+                                match RawPacket::try_from(Bytes::from(km_payload)) {
+                                    Ok(packet) => {
+                                        let attr = packet.get_attribute();
+                                        if total_packets <= 10 {
+                                            println!("Packet {}: attribute = {:?}", total_packets, attr);
+                                        }
+                                        
+                                        // Check if this is a PD packet
+                                        if attr == Some(Attribute::PdPacket) {
+                                            pd_packets += 1;
+                                            println!("Found PD packet! Payload length: {}", packet.get_payload_data().len());
+                                        }
+                                    }
+                                    Err(e) => {
+                                        eprintln!("Failed to parse KM packet: {}", e);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    _ => (),
+                }
+                reader.consume(offset);
+            }
+            Err(PcapError::Eof) => break,
+            Err(PcapError::Incomplete(_)) => {
+                reader.refill().unwrap();
+                continue;
+            }
+            Err(e) => panic!("Error: {:?}", e),
+        }
+    }
+
+    println!("Processed {} total KM packets", total_packets);
+    println!("Found {} PD packets", pd_packets);
+}
--- km003c-lib/src/packet.rs
+++ km003c-lib/src/packet.rs
@@ -145,11 +145,7 @@ impl RawPacket {
     pub fn get_attribute(&self) -> Option<Attribute> {
         match self {
             RawPacket::Ctrl { header, .. } => Some(Attribute::from_primitive(header.attribute())),
-            RawPacket::Data { .. } => {
-                // For Data packets, attribute comes from extended header
-                self.get_extended_header()
-                    .map(|ext_header| Attribute::from_primitive(ext_header.attribute()))
-            }
+            RawPacket::Data { extended, .. } => Some(Attribute::from_primitive(extended.attribute())),
         }
     }
 }
